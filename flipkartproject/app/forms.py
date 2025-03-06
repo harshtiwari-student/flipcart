91abe9c36f5884ea6912b40dec9fc29e6853e907
harshtiwari-student <tiwariharsh3939@gmail.com>
2025-03-06 08:40:05 +0000

    seventh commit

--- flipkartproject/app/forms.py
+++ flipkartproject/app/forms.py
@@ -1,9 +1,21 @@
 from django import forms
-from .models import Address
+from .models import Address,Product
 
 class AddressForm(forms.ModelForm):
     class Meta:
         model=Address
         fields=["mobile","address","pincode"]
 
+class ProductForm(forms.ModelForm):
+    class Meta:
+        Model=Product
+        fields=[
+            "userid",
+            "productid",
+            "productname",
+            "category",
+            "decription",
+            "price",
+            "images"
+        ]
 
